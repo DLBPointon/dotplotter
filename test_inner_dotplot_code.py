d0bacb2efb19dba725b3259a87643b62e48fd5e7
DLBPointon <damonlbp@hotmail.co.uk>
2019-12-17 15:05:41 +0000

    Addition of PyTests for the use of --code

---
+++ test_inner_dotplot_code.py
@@ -0,0 +1,6 @@
+from basics import inner_dotplot
+
+
+def test_result_of_inner_dotplot():
+	assert inner_dotplot('AB', 'AB', code=True) == ['\\', ' ', '|A\n',
+													 ' ', '\\', '|B\n']
---
+++ test_inner_dotplot_code2.py
@@ -0,0 +1,10 @@
+from basics import inner_dotplot
+
+
+def test_result_of_inner_dotplot2():
+	assert inner_dotplot('ABCDEF', 'ABCDEF', code=True) == ['\\', ' ', ' ', ' ', ' ', ' ', '|A\n',
+															' ', '\\', ' ', ' ', ' ', ' ', '|B\n',
+															' ', ' ', '\\', ' ', ' ', ' ', '|C\n',
+															' ', ' ', ' ', '\\', ' ', ' ', '|D\n',
+															' ', ' ', ' ', ' ', '\\', ' ', '|E\n',
+															' ', ' ', ' ', ' ', ' ', '\\', '|F\n']
